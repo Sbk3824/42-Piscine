03c9a6e7db85508bafbfc8f9a246645095bcbb33
Shantagouda Kuntoji <skuntoji@e1z1r3p4.42.us.org>
2018-06-27 18:59:18 -0700

    day04

--- day06/ex04/ft_sort_params.c
+++ day06/ex04/ft_sort_params.c
@@ -6,8 +6,63 @@
 /*   By: skuntoji <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/27 12:10:08 by skuntoji          #+#    #+#             */
-/*   Updated: 2018/06/27 12:12:57 by skuntoji         ###   ########.fr       */
+/*   Updated: 2018/06/27 13:45:19 by skuntoji         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putchar(char c);
 
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_print_param(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 1;
+	while (i < argc)
+		ft_print_param(argv[i++]);
+	return (0);
+}
